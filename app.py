24eae462fd559e5fe3558ca3190b712d81edfcd1
unknown <fanoprcs@gmail.com>
2022-07-04 23:10:50 +0800

    second_version

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from PIL import Image, ImageDraw, ImageFont
 from bs4 import BeautifulSoup
 app = flask.Flask(__name__)
 app.secret_key = "secret key"
-RGB = "none"
+
 word = "none"
 show_style = "none"
-word_color = "none"
-background_color = "none"
-
+wordsize = "none"
+# word_color = "#D68BBB"
+# background_color = "#000000"
 
 
 
@@ -17,44 +17,83 @@ background_color = "none"
 def home():
     global frequency #移動頻率
     frequency = 0
-    return flask.render_template('page.html', show_word_area = "輸入的文字為: " + word, show_type_area = "選擇的字體為: " + show_style)
+    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
 
 @app.route("/get_Word", methods=["POST"])
 def get_Word():
     global word
     word = flask.request.values.get('Word')
-    return flask.render_template('page.html', show_word_area = "輸入的文字為: " + word, show_type_area = "選擇的字體為: " + show_style)
+    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
+
+# @app.route('/get_Color',  methods=['GET', 'POST'])
+# def get_Color():
+#     global word_color, background_color
+#     word_color = flask.request.values.get('word-color')
+#     background_color = flask.request.values.get('background-color')
+#     return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
 
 @app.route("/set_mingliu")
 def set_mingliu():
     global show_style, font_style
     font_style = "mingliu.ttc"
     show_style = "新明細體"
-    return flask.render_template('page.html', show_word_area = "輸入的文字為: " + word, show_type_area = "選擇的字體為: " + show_style)
+    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
 
 @app.route("/set_msjh")
 def set_msjh():
     global show_style, font_style
     font_style = "msjh.ttc"
     show_style = "微軟正黑體"
-    return flask.render_template('page.html', show_word_area = "輸入的文字為: " + word, show_type_area = "選擇的字體為: " + show_style)
+    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
 
 @app.route("/set_kaiu")
 def set_kaiu():
     global show_style, font_style
     font_style = "kaiu.ttf"
     show_style = "標楷體"
-    return flask.render_template('page.html', show_word_area = "輸入的文字為: " + word, show_type_area = "選擇的字體為: " + show_style)
+    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
 
+@app.route("/set_24px")
+def set_24px():
+    global wordsize
+    wordsize = "24"
+    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
+@app.route("/set_36px")
+def set_36px():
+    global wordsize
+    wordsize = "36"
+    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
+@app.route("/set_48px")
+def set_48px():
+    global wordsize
+    wordsize = "48"
+    return flask.render_template('page.html', show_word_area = "輸入的文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
+@app.route("/set_64px")
+def set_64px():
+    global wordsize
+    wordsize = "64"
+    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
+@app.route("/set_72px")
+def set_72px():
+    global wordsize
+    wordsize = "72"
+    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
 
-word_color = (0,255,255)
-background_color = (255, 251, 240)
 
 @app.route("/start",  methods=['GET', 'POST'])
 def start():
-    global image
-    fontsize = 32
+    if word == "none" or word == "":
+        return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize , show_status_area = "請輸入文字")
+    if wordsize == "none":
+        return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize, show_status_area = "請選擇字體大小")
+    if show_style == "none":
+        return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize, show_status_area = "請選擇字體")
     
+    word_color = flask.request.values.get('word-color')
+    background_color = flask.request.values.get('background-color')
+    
+    global image
+    fontsize = int(wordsize)
     font = ImageFont.truetype(font_style, fontsize, encoding='utf-8')
     # RGB
     image = Image.new(mode = 'RGB', size = (fontsize * len(word), fontsize), color = background_color)
@@ -86,8 +125,9 @@ def frames():
             image[:,[i]] = image[:,[i+1]]
         image[:,[image.shape[1]-1]] = first
 
+        
 if __name__ == "__main__":
-    port = 3000
+    port = 3007
     link = "http://127.0.0.1:{0}".format(port)
     webbrowser.open(link)
     app.config['JSON_AS_ASCII'] = False
