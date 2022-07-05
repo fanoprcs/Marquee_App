5bcfa73216cc0d290b1171f6a7565bd100f3b58e
unknown <fanoprcs@gmail.com>
2022-07-06 03:27:34 +0800

    third version

--- app.py
+++ app.py
@@ -53,11 +53,7 @@ def set_kaiu():
     show_style = "標楷體"
     return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
 
-@app.route("/set_24px")
-def set_24px():
-    global wordsize
-    wordsize = "24"
-    return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize)
+
 @app.route("/set_36px")
 def set_36px():
     global wordsize
@@ -83,36 +79,51 @@ def set_72px():
 @app.route("/start",  methods=['GET', 'POST'])
 def start():
     if word == "none" or word == "":
-        return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize , show_status_area = "請輸入文字")
+        return flask.render_template('page.html', show_status_area = "請輸入文字")
     if wordsize == "none":
-        return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize, show_status_area = "請選擇字體大小")
+        return flask.render_template('page.html', show_status_area = "請選擇字體大小")
     if show_style == "none":
-        return flask.render_template('page.html', show_word_area = "輸入文字為: " + word, show_type_area = "選擇字體為: " + show_style, show_size_area = "字體大小為: " + wordsize, show_status_area = "請選擇字體")
+        return flask.render_template('page.html', show_status_area = "請選擇字體樣式")
     
+    global speed_rate
     word_color = flask.request.values.get('word-color')
     background_color = flask.request.values.get('background-color')
-    
+    speed_rate = flask.request.values.get('amountRange')
     global image
     fontsize = int(wordsize)
     font = ImageFont.truetype(font_style, fontsize, encoding='utf-8')
-    # RGB
-    image = Image.new(mode = 'RGB', size = (fontsize * len(word), fontsize), color = background_color)
-    draw = ImageDraw.Draw(image)
-    if font_style == "msjh.ttc":
-        draw.text((0, -1 * (fontsize/5)), word, fill = word_color, font=font)
-    else:
-        draw.text((0,0), word, fill = word_color, font=font)
-    image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
+    global unit, width
+    width = 288
+    height = 100
+    unit = 256 #位是當字撥放完後的留白像素量
     
+    if fontsize * len(word) >= width: #因為要讓跑馬燈畫面固定，所以要分成字體加總長度超過畫面寬和沒超過畫面寬兩總處理方式
+        image = Image.new(mode = 'RGB', size = (fontsize * len(word) + unit, height), color = background_color)
+        draw = ImageDraw.Draw(image)
+        if font_style == "msjh.ttc":
+            draw.text((unit/2, (height-fontsize)/ 2 - (fontsize / 5)), word, fill = word_color, font=font)
+        else:
+            draw.text((unit/2, (height-fontsize)/ 2), word, fill = word_color, font=font)
+    else:
+        image = Image.new(mode = 'RGB', size = (unit + (fontsize * len(word)), height), color = background_color)
+        draw = ImageDraw.Draw(image)
+        if font_style == "msjh.ttc":
+            draw.text((unit/2, (height-fontsize)/ 2 - (fontsize / 5)), word, fill = word_color, font=font)
+        else:
+            draw.text((unit/2, (height-fontsize)/ 2), word, fill = word_color, font=font)
+    image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
+
     return flask.render_template('execute.html')
 
 @app.route('/video_feed')
 def video_feed():
     return flask.Response(frames(), mimetype = 'multipart/x-mixed-replace; boundary=frame')
- ##移動速度
+
 def frames():
+    speed = int(speed_rate)+2
     while True:
-        frame = cv2.resize(image, None, fx = 960 / image.shape[0], fy= 960 / image.shape[0], interpolation = cv2.INTER_AREA)
+        frame = image[:, 0:width] #讓畫面大小固定
+        frame = cv2.resize(frame, None, fx = 960 / frame.shape[0], fy= 960 / frame.shape[0], interpolation = cv2.INTER_AREA)
         try:
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
@@ -120,10 +131,13 @@ def frames():
                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         except Exception as e:
             pass
-        first = image[:,[0]]
-        for i in range(image.shape[1]-1):
-            image[:,[i]] = image[:,[i+1]]
-        image[:,[image.shape[1]-1]] = first
+        first = image[: ,0:speed].copy()
+        for i in range(image.shape[1]):
+            if i + speed >= image.shape[1]:
+                image[:, [i]] = image[:, [i+speed - image.shape[1]]]
+                continue
+            image[:, [i]] = image[:, [i+speed]]
+            
 
         
 if __name__ == "__main__":
